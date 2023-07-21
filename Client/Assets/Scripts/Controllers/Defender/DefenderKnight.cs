597dd75a0a091b884a917c2d3b5c6bfa812d9186
daegee <kimdae963@catholic.ac.kr>
2023-07-21 16:13:07 +0900

    Update : Defender Scripts

---
+++ Client/Assets/Scripts/Controllers/Defender/DefenderKnight.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderKnight : DefenderController
+{
+    protected override void Start()
+    {
+        base.Start();
+        _attRange = (float)Define.AttRange.Knight / 30;
+        _range = _attRange * 2;
+    }
+
+    IEnumerator CoStartAttack()
+    {
+        _character.Animator.SetTrigger("Attack");
+        yield return new WaitForSeconds(1.0f);
+        _coAttack = null;
+    }
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.tag != tag && _coAttack == null)
+        {
+            _coAttack = StartCoroutine("CoStartAttack");
+        }
+    }
+}
