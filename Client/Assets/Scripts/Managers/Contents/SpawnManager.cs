481d373b16e8d45fd285808ae81173b0b2ef839d
rabbitmjh <rabbitmjh@naver.com>
2023-07-28 04:41:19 +0900

    Update : Monster Spawn Range
    x: -2.5 ~ -2.0 y: -2.5 ~ 2.5
    x: +2.0 ~ +2.5 y: -2.5 ~ 2.5
    x: -2.0 ~ +2.0 y: -2.5 ~ -2.0
    x: -2.0 ~ +2.0 y: +2.0 ~ +2.5

--- Client/Assets/Scripts/Managers/Contents/SpawnManager.cs
+++ Client/Assets/Scripts/Managers/Contents/SpawnManager.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnManager
+public class SpawnManager : MonoBehaviour
 {
+    private Coroutine _spBlock;
+
     [SerializeField]
     private int[] monsterType = new int[6] { 10, 11, 20, 21, 30, 31 };
-    Vector3[] monsterSpawnPos = new Vector3[4];
+    private Vector3[] monsterSpawnPos = new Vector3[4];
+    private Vector3 monsterSpawn;
+    private float _spCoolDown = 0.0f;
+    private float _spCoolTime = 2.0f;
+    private Define.MonsterType monType;
 
     public void SpawnMonster(Define.MonsterType type)
     {
@@ -18,8 +24,11 @@ public class SpawnManager
         monsterSpawnPos[2] = new Vector3(Random.Range(playerPos.x - 2.0f, playerPos.x + 2.0f), Random.Range(playerPos.y - 2.5f, playerPos.y - 2.0f), 0);
         monsterSpawnPos[3] = new Vector3(Random.Range(playerPos.x - 2.0f, playerPos.x + 2.0f), Random.Range(playerPos.y + 2.0f, playerPos.y + 2.5f), 0);
 
-        Vector3 spawnPos = monsterSpawnPos[Random.Range(0, 4)];
-        Managers.Monster.MakeMonster(type, monsterType[Random.Range(0, 6)], spawnPos);
+        monsterSpawn = monsterSpawnPos[Random.Range(0, 4)];
+        if (_spBlock == null && _spCoolDown < 0)
+        {
+            _spBlock = StartCoroutine("SpBlock");
+        }
     }
     public void SpawnDefender(Define.DefenderType type)
     {
@@ -28,5 +37,13 @@ public class SpawnManager
         
         Vector3 spawnPos = new Vector3(Random.Range(playerPos.x - 0.5f, playerPos.x + 0.5f), Random.Range(playerPos.y - 0.5f, playerPos.y + 0.5f), 0);
         Managers.Defender.MakeDefender(type, spawnPos);
+    }
+
+    IEnumerator SpBlock()
+    {
+        Managers.Monster.MakeMonster(monType, monsterType[Random.Range(0, 6)], monsterSpawn);
+        yield return new WaitForSeconds(0.1f);
+        _spBlock = null;
+        _spCoolDown = _spCoolTime;
     }
 }
