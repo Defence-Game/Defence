666fd0fbf87ef40cd2b1025fe29fede38fc7c2a8
daeheee <kimdae963@gmail.com>
2023-08-02 22:32:06 +0900

    Update : UI_button defender 소환

--- Client/Assets/Scripts/UI/UI_DefenderCool.cs
+++ Client/Assets/Scripts/UI/UI_DefenderCool.cs
@@ -1,40 +1,105 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class UI_DefenderCool : MonoBehaviour
+public class UI_DefenderCool : UI_Base
 {
-    public Image skillFilter;
+    //public Image[] skillFilter = new Image[3];
+    public List<Image> skillFilter = new List<Image>();
     public Text coolTimeCounter; //남은 쿨타임을 표시할 텍스트
+    public Button _btn;
 
     public float coolTime;
 
     private float currentCoolTime; //남은 쿨타임
 
     private bool canSummon = true;
-    
-    void start()
+    private PlayerController pc;
+
+    enum Buttons
     {
-        skillFilter.fillAmount = 0; //처음에 스킬 버튼을 가리지 않음
+        UI_Knight,
+        UI_Archer,
+        UI_Mage,
     }
 
-    public void DefenderBtn()
+    enum Images
     {
-        if (canSummon)
+        Knight_fillter,
+        Archer_fillter,
+        Mage_fillter,
+    }
+
+    public override void Init()
+    {
+        pc = BaseScene.player.GetComponent<PlayerController>();
+        coolTime = pc._defenderCoolTime;
+
+        Bind<Button>(typeof(Buttons));
+        Bind<Image>(typeof(Images));
+
+        skillFilter.Add(GetImage((int)Images.Knight_fillter));
+        skillFilter.Add(GetImage((int)Images.Archer_fillter));
+        skillFilter.Add(GetImage((int)Images.Mage_fillter));
+
+        GetButton((int)Buttons.UI_Knight).gameObject.BindEvent(SummonDefender);
+        GetButton((int)Buttons.UI_Archer).gameObject.BindEvent(SummonDefender);
+        GetButton((int)Buttons.UI_Mage).gameObject.BindEvent(SummonDefender);
+    }
+
+    public void Update()
+    {
+        if (pc._gold >= pc._summonGold && pc._defenderCoolDown < 0)
+        {
+            if (Input.GetKey(KeyCode.Z))//Defender Knight summon
+            {
+                SummonDefender("UI_Knight");
+            }
+            if (Input.GetKey(KeyCode.X))//Defender Archer summon
+            {
+                SummonDefender("UI_Archer");
+            }
+            if (Input.GetKey(KeyCode.C))//Defender Mage summon
+            {
+                SummonDefender("UI_Mage");
+            }
+        }
+    }
+    private void SummonDefender(string name)
+    {
+        if (canSummon && pc._gold >= pc._summonGold)
         {
             GameObject player = GameObject.Find("Player");
             Vector3 playerPos = player.transform.position;
             Vector3 spawnPos = new Vector3(Random.Range(playerPos.x - 0.5f, playerPos.x + 0.5f), Random.Range(playerPos.y - 0.5f, playerPos.y + 0.5f), 0);
-            Managers.Defender.MakeDefender(Define.DefenderType.Archer, spawnPos);
 
-            skillFilter.fillAmount = 1;
+            switch (name)
+            {
+                case "UI_Archer":
+                    Managers.Defender.MakeDefender(Define.DefenderType.Archer, spawnPos);
+                    break;
+                case "UI_Knight":
+                    Managers.Defender.MakeDefender(Define.DefenderType.Knight, spawnPos);
+                    break;
+                case "UI_Mage":
+                    Managers.Defender.MakeDefender(Define.DefenderType.Mage, spawnPos);
+                    break;
+            }
+
+            for (int i = 0; i < skillFilter.Count; i++)
+            {
+                skillFilter[i].fillAmount = 1;
+            }
+
             StartCoroutine("Cooltime");
 
             currentCoolTime = coolTime;
-            coolTimeCounter.text = "" + currentCoolTime;
 
             StartCoroutine("CoolTimeCounter");
-
+            pc._gold -= pc._summonGold;
             canSummon = false;
         }
         else
@@ -42,13 +107,55 @@ public class UI_DefenderCool : MonoBehaviour
             Debug.Log("소환이 불가능합니다.");
         }
     }
+    private void SummonDefender(PointerEventData data)
+    {
+        string name = data.pointerClick.name;
 
+        if (canSummon && pc._gold >= pc._summonGold)
+        {
+            GameObject player = GameObject.Find("Player");
+            Vector3 playerPos = player.transform.position;
+            Vector3 spawnPos = new Vector3(Random.Range(playerPos.x - 0.5f, playerPos.x + 0.5f), Random.Range(playerPos.y - 0.5f, playerPos.y + 0.5f), 0);
+
+            switch (name)
+            {
+                case "UI_Archer":
+                    Managers.Defender.MakeDefender(Define.DefenderType.Archer, spawnPos);
+                    break;
+                case "UI_Knight":
+                    Managers.Defender.MakeDefender(Define.DefenderType.Knight, spawnPos);
+                    break;
+                case "UI_Mage":
+                    Managers.Defender.MakeDefender(Define.DefenderType.Mage, spawnPos);
+                    break;
+            }
+
+            for (int i = 0; i < skillFilter.Count; i++)
+            {
+                skillFilter[i].fillAmount = 1;
+            }
+            
+            StartCoroutine("Cooltime");
+
+            currentCoolTime = coolTime;
+            
+            StartCoroutine("CoolTimeCounter");
+            pc._gold -= pc._summonGold;
+            canSummon = false;
+        }
+        else
+        {
+            Debug.Log("소환이 불가능합니다.");
+        }
+    }
     IEnumerator Cooltime()
     {
-        while (skillFilter.fillAmount > 0)
+        while (skillFilter[0].fillAmount > 0)
         {
-            skillFilter.fillAmount -= 1 * Time.smoothDeltaTime / coolTime;
-
+            for (int i = 0; i < skillFilter.Count; i++)
+            {
+                skillFilter[i].fillAmount -= 1 * Time.smoothDeltaTime / coolTime;
+            }
             yield return null;
         }
 
@@ -64,7 +171,6 @@ public class UI_DefenderCool : MonoBehaviour
             yield return new WaitForSeconds(1.0f);
 
             currentCoolTime -= 1.0f;
-            coolTimeCounter.text = "" + currentCoolTime;
         }
 
         yield break;
